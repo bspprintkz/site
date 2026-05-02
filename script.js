0190101ec1c2b9fc98fb19de826dd4303c14266e
bspprintkz <bspprint.kz@gmail.com>
2026-05-02 17:56:24 +0500

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
 const gallery = document.getElementById("gallery");
 
-async function loadImages() {
-  const res = await fetch("https://site-zeta-orpin.vercel.app/api/images");
-  const data = await res.json();
+const test = [
+  "https://res.cloudinary.com/demo/image/upload/sample.jpg"
+];
 
-  data.resources.forEach(item => {
-    const img = document.createElement("img");
-
-    img.src = item.secure_url;
-
-    img.onclick = () => openLightbox(img.src);
-
-    gallery.appendChild(img);
-  });
-}
-
-function openLightbox(src) {
-  const lightbox = document.getElementById("lightbox");
-  const img = document.getElementById("lightbox-img");
-
+test.forEach(src => {
+  const img = document.createElement("img");
   img.src = src;
-  lightbox.style.display = "flex";
-}
-
-document.getElementById("lightbox").onclick = () => {
-  document.getElementById("lightbox").style.display = "none";
-};
-
-loadImages();
+  gallery.appendChild(img);
+});
